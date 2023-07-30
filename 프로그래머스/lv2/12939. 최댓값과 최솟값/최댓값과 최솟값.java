bd13bc3069ec33918389f957a2a9343b3d249fb2
raeunlee <49014085+raeunlee@users.noreply.github.com>
2023-07-30 16:54:57 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 9.81 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        
+        String str[] = s.split(" ");
+        
+        for(int i = 0; i < str.length; i++){
+            arr.add(Integer.parseInt(str[i]));
+        }
+        
+        int max = Collections.max(arr);
+        int min = Collections.min(arr);
+        
+        answer =  "" + min + " " +max;
+        
+        return answer;
+    }
+}
