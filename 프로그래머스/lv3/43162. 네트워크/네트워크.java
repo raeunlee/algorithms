080232db267a0533124ef18bcea35151ffeda433
raeunlee <49014085+raeunlee@users.noreply.github.com>
2023-09-19 21:39:11 +0900

    [level 3] Title: 네트워크, Time: 0.16 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/lv3/43162. 네트워크/네트워크.java
+++ 프로그래머스/lv3/43162. 네트워크/네트워크.java
@@ -1,29 +1,44 @@
+import java.util.*;
+
 class Solution {
+    Queue<Integer> q = new LinkedList<>();
     public int solution(int n, int[][] computers) {
         int answer = 0;
-        boolean[] visited = new boolean[computers.length];
-        
-        for(int i = 0; i < computers.length; i++){
-            visited[i] = false;
-        }
+        boolean[] visited = new boolean[n]; 
         
         for(int i =0; i < computers.length; i++){
-            if(visited[i] == false) {
+            if(visited[i] == false) { // default
+                bfs(i, visited, computers);     
                 answer ++;
-                dfs(i, visited, computers);
             }
         }
-        
         return answer;
     }
     
-    void dfs(int com, boolean[] visited, int[][] computers){
-        visited[com] = true; // 방문 true
-        
-        for(int i = 0; i < computers.length; i++){
-            if(visited[i] == false && computers[com][i]==1){
-                dfs(i, visited, computers); //방문안하고 1이면
+   void bfs(int com, boolean[]visited, int[][] computers){
+        q.offer(com);
+        visited[com] = true;
+        while(!q.isEmpty()){
+            int value = q.poll();// 빼낸게 
+            for(int i = 0; i < computers.length; i ++) { // 네트워크만큼 ㄱ
+                if (visited[i] == false && computers[value][i]==1){
+                    //1이거나 방문안한거있으면 
+                    visited[i] = true; // 방문처리 
+                    q.add(i); // 다시 더해준다,, 무한반복
+                }
             }
         }
     }
+    
+//     void dfs(int com, boolean[] visited, int[][] computers){
+//         visited[com] = true; // 방문 true
+        
+//         for(int i = 0; i < computers.length; i++){
+//             if(visited[i] == false && computers[com][i]==1){
+//                 dfs(i, visited, computers); //방문안하고 1이면
+//             }
+//         }
+//     }
+    
+
 }
