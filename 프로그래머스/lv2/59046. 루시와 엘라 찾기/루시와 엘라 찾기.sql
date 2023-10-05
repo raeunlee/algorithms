e3d245b1bf70af9301352912d12fbd8faaccb66e
raeunlee <49014085+raeunlee@users.noreply.github.com>
2023-10-05 16:40:09 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,9 +1,4 @@
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME LIKE 'LUCY' OR 
-        NAME LIKE 'Ella' OR
-        NAME LIKE 'Pickle' OR
-        NAME LIKE 'Rogan' OR
-        NAME LIKE 'Sabrina' OR
-        NAME LIKE 'Mitty' 
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
