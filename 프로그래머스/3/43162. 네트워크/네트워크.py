0ee02ff41424134cbda413e4acb2f85ed3c2b3f0
raeunlee <49014085+raeunlee@users.noreply.github.com>
2024-01-04 09:05:50 +0900

    [level 3] Title: 네트워크, Time: 1.35 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,15 +1,20 @@
+def dfs(n, computers, visited):
+    visited[n] = True #현재 컴퓨터 방문처리
+    tmp = len(computers)
+    #현재 컴퓨터랑 연결된 컴퓨터들을 탐색
+    for i in range(tmp):
+        print(computers[n][i])
+        if computers[n][i] == 1 and not visited[i]: #방문하지 않고, 연결되어 있다면?
+            print("y")
+            dfs(i, computers, visited) #다시 반복한다
+
 def solution(n, computers):
-    network = 0
-    visited = [False for i in range(n)]
-    for num in range(n):
-        if visited[num] == False:
-            DFS(n, computers, num, visited)
-            network +=1
-    return network
-            
-def DFS(n, computers, num, visited):
-    visited[num] = True
-    for cnt in range(n):
-        if num!=cnt and computers[num][cnt]==1:
-            if visited[cnt]==False:
-                DFS(n, computers, cnt, visited)
+    answer = 0
+    visited = [False for _ in range(n)] # 방문 체크 배열
+    
+    for i in range(n): #네트워크 하나씩 탐색 
+        if not visited[i]: #해당 네트워크를 방문한적이 없다면
+            print(i)
+            dfs(i, computers, visited) # 재귀시작
+            answer += 1            
+    return answer
