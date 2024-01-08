69228b752edd75e94e2bbc4448e1068a7e3ed8d9
raeunlee <49014085+raeunlee@users.noreply.github.com>
2024-01-08 09:12:09 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.11 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -1,35 +1,28 @@
 from collections import deque
 
 def solution(maps):
-    r = len(maps)
-    c = len(maps[0])
-
-    queue = deque()
-    queue.append([0,0])        
-    while queue:
-        # x(행),y(열) 좌표 받기
-        x,y = queue.popleft()
-
-        # 방향 벡터(상,하,좌,우)로 탐색
-        dx = [1,-1,0,0]
-        dy = [0,0,-1,1]
+    sero = len(maps)
+    garo = len(maps[0])
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    
+    q = deque([(0, 0)])
+    
+    # 1로만 다닐 수 있음
+    while q:
+        x, y = q.popleft()
+        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            # 범위를 벗어나는 경우 무시
-            if nx < 0 or ny < 0 or nx >= r or ny >= c: 
-                continue
-            # 벽인 경우 무시
-            if maps[nx][ny] == 0: 
-                continue
-            # 방문한 적이 없는 경우에만
-            if maps[nx][ny] == 1:
-                # 이전 좌표값 +1
+            
+            # 다음 좌표가 1이고, 범위를 넘지 않는다면? 
+            if 0 <= nx < sero and 0 <= ny < garo and maps[nx][ny] == 1:
                 maps[nx][ny] = maps[x][y] + 1
-                # 큐에 새로 추가
-                queue.append([nx,ny])
-
-    # 상대 진영에 방문한 적이 없다면
-    if maps[r-1][c-1] == 1: return -1
-    # 상대 진영에 방문했다면 기록 반환
-    else: return maps[r-1][c-1]
+                q.append((nx, ny))
+    
+    if maps[sero-1][garo-1] == 1:
+        return -1
+    else:
+        return maps[sero-1][garo-1]
