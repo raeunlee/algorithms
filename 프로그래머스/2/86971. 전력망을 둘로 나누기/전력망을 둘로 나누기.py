ca198c4209f1a4a1119f9116b6513576efa25af4
raeunlee <49014085+raeunlee@users.noreply.github.com>
2024-01-09 09:40:56 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 1.18 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,34 +1,34 @@
 from collections import deque
 
-def bfs(start,visited,graph):
-    queue = deque([start]) #시작점
-    result = 1
-    visited[start] = True #방문처리
-    while queue:
-        now = queue.popleft() #현재지점
-        
-        for i in graph[now]:
-            if visited[i] == False: 
-                result += 1
-                queue.append(i)
+def bfs(start, visited, graph ):
+    q = deque([start])
+    cnt = 1
+    visited[start] = True
+    while q:
+        x = q.popleft() # 현재 지점
+        for i in graph[x]: # graph[x] 안의 요소들
+            if visited[i] == False: # 방문하지 않은 수라면 
+                cnt += 1 # 카운드 세준다 
+                q.append(i)
                 visited[i] = True
-                
-    return result
+    return cnt # cnt return 
         
+
 
 def solution(n, wires):
     answer = n
-    graph = [[] for _ in range(n+1)]
+    graph = [[] for _ in range(n+1)]  
+    for a, b in wires:
+        graph[a].append(b)
+        graph[b].append(a)
     
-    for v1,v2 in wires:
-        graph[v1].append(v2)
-        graph[v2].append(v1)
+    print(graph)
+    
+    for start, end in wires:
+        visited = [False] * (n+1)
+        visited[end] = True
+        result = bfs(start, visited, graph)
+        if abs(result - ( n - result)) < answer:
+            answer = abs(result - (n - result))
             
-    for start, not_visit in wires: #시작점, 방문안한점 
-        visited = [False]*(n+1)
-        visited[not_visit] = True 
-        result = bfs(start,visited,graph) 
-        if abs(result - (n-result)) < answer:
-            answer = abs(result - (n-result))
-        
     return answer
