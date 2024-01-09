73f420b5c5eb85dc26fa6ce552889a282610a57d
raeunlee <49014085+raeunlee@users.noreply.github.com>
2024-01-09 09:05:10 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/README.md
+++ 프로그래머스/2/42883. 큰 수 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 큰 수 만들기 - 42883 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42883) 
 
 ### 성능 요약
 
-메모리: 10.3 MB, 시간: 0.00 ms
+메모리: 10.3 MB, 시간: 0.01 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 10월 2일 16:18:40
+2024년 1월 2일 9:5:8
 
 ### 문제 설명
 
--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.py
@@ -1,24 +1,23 @@
 def solution(number, k):
     answer = ''
-    tmp = [] # 만들 수 
+    tmp = []
     
     for n in number:
-        if len(tmp) == 0: #비어있다면 넣어준다
+        #숫자 배열이 비어있으면 하나 넣어준다
+        if len(tmp) == 0:
             tmp.append(n)
             continue
-        if k > 0:        
-            while tmp[-1] < n: #맨 위가 number의 숫자 보다 작다면?
+        if k > 0: #제거할 숫자가 0이상이면
+            while tmp[-1] < n: # 맨 위가 현재 숫자보다 작다면
                 tmp.pop()
-                k -= 1
-                if len(tmp) == 0 or k<=0:
-                    break
+                k -= 1 # 제거할 숫자도 줄어듦
+                if len(tmp) == 0 or k == 0:
+                    break #tmp가 0이거나 k가 0이면 break
         tmp.append(n)
-    
+        
     if k > 0:
-        tmp = tmp[:-k] #스택 맨 위부터 차례로
-        
-        
-    
-    
-   
+        tmp = tmp[:-k]
+    else:
+        tmp
     return ''.join(tmp)
+            
