ebb3c2e44197ae2482a4f1d2649f38918cbb2187
raeunlee <raeun19@naver.com>
2024-09-03 23:04:18 +0900

    [level 2] Title: 카펫, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,25 +1,13 @@
 def solution(brown, yellow):
-    answer = []    
-    gx = 0
-    gy = 0
+    answer = []
     
-    #갈색카펫 가로는 최소 3부터 brown /2 - 1 이 최대
-    for i in range(3, int(brown/2)):
-        gx = i #갈색 가로        
-        
-        #세로는 가로길이 만큼이 최대
-        for j in range(1, gx+1):
-            gy = j
-            
-            brown_gatsu = gx*2 + gy*2 - 4 # 갈색 갯수
-            yellow_gatsu = (gx-2) * (gy-2)
-            all_gatsu = brown_gatsu + yellow_gatsu
-            
-            if brown_gatsu == brown and yellow_gatsu == yellow:
-                if all_gatsu == brown + yellow:
-                    print("yes")
-           
-                    answer.append(gx)
-                    answer.append(gy)
-
+    total = brown + yellow
+    
+    for sero in range(1, brown//2 + 1):
+        if total % sero == 0:
+            garo = total // sero
+            if (garo - 2) * (sero - 2) == yellow:
+                if garo >= sero:
+                    answer.append(garo)
+                    answer.append(sero)
     return answer
