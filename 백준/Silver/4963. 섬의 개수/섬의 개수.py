7a600dacf1453ceab9953ae78ad445aff0ae2d14
raeunlee <raeun19@naver.com>
2024-10-27 00:21:37 +0900

    [Silver II] Title: 섬의 개수, Time: 172 ms, Memory: 114868 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,47 +1,48 @@
-#섬의 개수
-import sys
-from collections import deque
-input = sys.stdin.readline
+# 50 * 50 * 50 * 4 = 5000000
 
-# 위 아래 양 옆 대각선 4방향
+from collections import deque
+
+# 위 아래 양 옆 대각선 8방향
 dx = [-1,1,0,0,-1,-1,1,1]
 dy = [0,0,-1,1,1,-1,1,-1]
 
-def bfs(x,y):
-    visited[x][y] = True
-    
-    q = deque()
-    q.append([x,y])
-    
-    while q:
-        x,y = q.popleft()
-        for i in range(8):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if 0<=nx<h and 0<=ny<w:
-                if visited[nx][ny]== False and land[nx][ny]==1:
-                    visited[nx][ny] = True
-                    q.append((nx,ny))
-                    
-    
-    
-    
-
-
 while True:
     w, h = map(int, input().split())
+    
     if w == 0 and h == 0:
-        break
-    land = []
-    visited = [ [False] * w for _ in range(h)]
-    answer = 0
-    for i in range(h):
-        land.append(list(map(int, input().split())))
-    for x in range(h):
-        for y in range(w):
-            if land[x][y] == 1 and visited[x][y] == False:
-                bfs(x,y)
-                answer += 1
+        exit(0)
+        
+    else:
+        graph = []
+        q = deque()
+        
+        for i in range(h):
+            row = list(map(int, input().split()))
+            graph.append(row)
+            
+        visited = [[False] * w for _ in range(h)]
+        
+        count = 0 
+        
+        for j in range(h):
+            for i in range(w):
+                if graph[j][i] == 1 and not visited[j][i]:
+                    q.append((j,i))
+                    visited[j][i] = True
+                    while q:
+                        y, x = q.popleft()
+                        for i in range(8):
+                            nx = x + dx[i]
+                            ny = y + dy[i]
+                            if 0 <= ny < h and 0 <= nx < w and not visited[ny][nx] and graph[ny][nx] == 1:
+                                q.append((ny, nx))
+                                visited[ny][nx] = True
+                    count += 1
+                    
+        print(count)
+                    
+                        
+                    
                 
-    print(answer)
+        
+        
